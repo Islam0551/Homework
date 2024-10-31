5de437ace8a4954fd6a8fdbcdd1b1bcfdf530c32
Islam <islam20110811p@gmail.com>
2024-10-31 14:40:28 +0500

    30.10

---
+++ 30.10/script.js
@@ -0,0 +1,11 @@
+let modelName = "Matiz";
+let color = "green-yellow";
+let wheelsCount = 4;
+let isOffRoad = false;
+let rating = 3/10; 
+if (isOffRoad =false)
+console.log("modelName:" , modelName);
+console.log(" color:", color);
+console.log(" wheelsCount:", wheelsCount);
+console.log("isOffRoad:", isOffRoad)
+console.log(" rating:" , rating);
