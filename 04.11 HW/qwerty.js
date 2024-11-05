081f94f6ecd39c899409a04810e78d803a763a4e
Islam <islam20110811p@gmail.com>
2024-11-05 14:22:35 +0500

    04.11 HW

---
+++ 04.11 HW/qwerty.js
@@ -0,0 +1,37 @@
+let age = 18; 
+if (age >= 16) {
+    console.log("Жасы 16-дан асқан");
+} else {
+    console.log("Жасы 16-дан аспаған");
+}
+
+
+let a = 7; 
+if (a % 2 === 0) {
+    console.log("жұп");
+} else {
+    console.log("тақ");
+}
+
+let temperature = 25; 
+if (temperature >= 20 && temperature <= 30) {
+    console.log("Температура 20-дан 30 градусқа дейін");
+} else {
+    console.log("Температура диапазонда емес");
+}
+
+
+let num = 5; 
+if (num > 0) {
+    console.log("Сан оң");
+} else {
+    console.log("Сан оң емес");
+}
+
+
+let b = 50; // Example number
+if (b % 5 === 0 && b % 10 === 0) {
+    console.log("шын");
+} else {
+    console.log("жалған");
+}
---
+++ ex3/script.js
@@ -0,0 +1,80 @@
+//var a=5
+//var b=3
+//
+//console.log(a==b)
+//
+//var a=5
+//var b=3
+//
+//console.log(a!=b)
+
+//var a=5;
+//var b=20;
+
+//console.log(a>b);//f
+//console.log(a<b);//t
+//console.log(a>=b);//f
+//console.log(a<=b);//t
+
+//let a = 4;
+//let b = 3;
+//let c = 10;
+//
+//console.log(a > b && c > a);
+//
+//let isAvailable =true
+//
+//console.log(!isAvailable);
+
+//let movie ='Venom';
+//let uaqyt =9;
+//let barama =true;
+//
+//if (movie=="Venom" && uaqyt <= 8){
+//    console.log(barama);
+//}else{
+//    console.log(!barama);
+//}
+//
+//let age = 18;
+//let hasTicket= false;
+//let isVip =true;
+//
+//if ((age<=18 && hasTicket) || isVip){
+//    console.log("Kiredi!");
+//}else{
+//    console.log("kirmeidi");
+//}
+
+// let age = 30;
+// let a = 12;
+// let b = 45;
+// 
+// console.log(age> a &&  age>b);
+
+
+//n = 21;
+//
+//console.log((n > 20 && n<50) && n%2==0);
+
+//var a = 39;
+//var b = 39;
+//var p=(a+b)*2
+//
+//console.log(p);
+//console.log((p >=30) && (p >=40) && (a==b));
+//
+//console.log ((a % 3==0) || (b % 5==0));
+
+
+
+//var temp1 = 20;
+//var temp2 = 35;
+//
+//console.log((temp1 >= 15 &&  temp1 <= 25 ) && (temp2 >30));
+//
+//var price = 5000;
+//var discount = 10;
+//var finalPrice = price - discount ;
+//console.log(finalPrice);
+//console.log(price >= 50);
