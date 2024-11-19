b29e503786213570062c78fd61700ea6eecd126f
Islam <islam20110811p@gmail.com>
2024-11-19 14:24:47 +0500

    HW 18.11

---
+++ EX 6/qwerty.js
@@ -0,0 +1,380 @@
+// for (let i = 2 ; i<=10 ; i+=2){
+//     if (i % 2 === 0){
+//         console.log(i + " - Жұп");
+//     }else {
+//         console.log((i + " -  Тақ"));
+//     }
+// }
+
+// for (let i = 1; i <5; i++){
+//     console.log(i);
+//     if (i == 5){
+//         break;
+// }
+// }
+
+// for (let i = 1; i <= 5 ; i++){
+//     if (i === 3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//  for (let i = 10; i > 1; i --){
+//     console.log(i);
+
+//  }
+
+// for ( let i = 0 ; i < 50; i ++ ){
+//     console.log(i);
+
+// }
+
+// let nam1 = Number (prompt("enter numder"));
+// let results = 0;
+
+// for (let i =0 ; i < nam1 ; i ++){
+// alert( i +  "+" + nam1 );
+// results +=i;
+
+// console.log(results);
+
+// console.log(i);
+// }
+
+// console.log("...");
+
+// let nam1 = Number(prompt("Entur Number"));
+// let results = 0;
+
+// for (let i=1; i < nam1 ; i++){
+//     alert(i + "*" + nam1 );
+//     results +=1;
+//     console.log(nam1 + "*" + i + "=" + (results * nam1));
+// }
+
+//  for ( let i = 1 ; 1 <= 3 ; i ++){
+// var suraq = prompt ("Qazaq handygy qai jyly quryldy")
+//  if (suraq == "1465"){
+//      alert("Durys");
+//      break;
+//  }else{
+//     alert('qate')
+//  }
+//  }
+
+// for (let i = 100; i >= -2; i+=0){
+//     console.log(i);
+//
+
+// for (let i = 1 ; i < 20 ; i ++ ){
+//     if (i > 5 && i < 15){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let count = o;
+
+// while ( count < 5){
+//     console.log(count);
+//     count ++;
+// }
+
+// let count = 0;
+
+// do {
+//     console.log(count);
+//     count ++;
+// }while(count < 5);
+
+// let num = 0;
+// while (num < 10){
+//     if (num ==5){
+//         console.log("San tabuldy" , num);
+//         break;
+//     }
+//     console.log(num);
+//     num ++;
+// }
+
+// let count = 0;
+
+// while (count < 15){
+//     count ++;
+
+//     if(count % 2 == 0){
+//         continue;
+//     }
+// }
+
+// let num = 1;
+// while(num < 10 ){
+//     if (num % 2 == 0){
+//         console.log(num + " - jyp san");
+
+//     }else{
+//         console.log(num + "-  tak san");
+
+//     }
+// }
+
+// let num = 1;
+// while (num<=15) {
+//     if (num%3==0&&num%5==0) {
+//         console.log('fizzbuzz');
+//     }else if(num%3==0){
+//         console.log('fizz');
+//     }else if (num %5 ==0){
+//         console.log('buzz');
+//     }else{
+//         console.log(num);
+
+//     }
+
+// }
+
+// let num = 1;
+// let num1 = 0;
+
+// while(num <= 10){
+//     num1 += num;
+//     num++;
+// }
+// console.log(num1);
+
+// let num = 1 ;
+// let num2 = 0 ;
+// let num3 = 0 ;
+
+// while (num<=10) {
+
+// if (num%2==0){
+//   num3+=num;
+//  }
+//  else {
+//  num2+=num
+//  }
+//   num++;
+//  }
+// console.log(num2);
+// console.log(num3);
+
+// let num ;
+
+// do {
+//     num = +prompt("Entur Numder");
+// }while (num < 0);
+
+// var  nam1;
+// var san = 5;
+// do {
+//     nam1 = prompt("АҚШ презтденті кім ?");
+//     if (nam1 == "Tramp"){
+//     alert("Durys");
+// alert(san)
+//           break;
+//       }else{
+//          alert('qate');
+//          san --;
+//       }
+//     }while (true)
+
+// alert (san);
+
+// var nam1 ;
+// var nam2 ;
+// var nam3 ;
+// var san = 5;
+
+// do {
+//     nam1 = prompt ("Қазақстан презтденті кім ?");
+//     if ( nam1 == "Тоқаев"){
+//         alert("Durys");
+//         alert(san);
+//     }else {
+//         alert('qate');
+//         san --;
+//     }while(true)
+//     nam2 = prompt ("Кайсардын курткасы кандай цвет ?");
+//     if ( nam3 == "Blue"){
+//        alert("Durys");
+//             alert(san);
+//         }else {
+//             alert('qate');
+//             san --;
+//         }while(true)
+//     nam3 = prompt ("Бекада кандай телефон ?");
+
+//     if ( nam3 == "Iphone 15 pro"){
+//             alert("Durys");
+//             alert(san);
+//             break;
+//         }else {
+//             alert('qate');
+//             san --;
+//         }while(true)
+// }while (true)
+
+// let  num = 0;
+// let results = 0;
+
+// while(num <= 10){
+//     results = results + num ;
+//     num ++ ;
+// }
+
+// console.log(results);
+
+// let num = 1 ;
+// let num2 = 0 ;
+// let num3 = 0 ;
+
+//  while (num<=10) {
+
+//  if (num%2==0){
+//      num3+=num;
+//   }
+//     else {
+//     num2+=num
+//   }
+//      num++;
+//   }
+//     console.log('Бұл жұп сан сандар косындысы $(num2)');
+//      console.log('Бұл жұп сан сандар косындысы $(num3)');
+
+// let num1 = 0;
+// let num2 = 0;
+
+// for (let i = 1; i <= 20 ; i++){
+//     if ( i % 2 == 0){
+//         num1 += i;
+//     }else{
+//         num2 += i;
+//     }
+// }
+
+// console.log(num1);
+// console.log(num2);
+
+// var number1 = Number(prompt('san jaz'))
+// var number2 = Number(prompt('san jaz'))
+// let num = 0
+// var num2 = 0
+
+// for (let i = number1; i <= number2; i++ ){
+//        if(i%2==0){
+//            num2+=i
+//            continue;
+//        }else{
+//            num+=i
+//            continue;
+//        }
+
+//    }
+//    console.log(num);
+//        console.log(num2);
+
+// let start = +prompt("Entur fistr number: ")
+// let end = +prompt("Entur second numder:")
+// let minNumder;
+// let maxNumder;
+// let resultsEven = 0;
+// let resultsOdd =0;
+
+// if (start > end ){
+//     maxNumder = start ;
+//     minNumder = end ;
+// }else{
+//     maxNumder = end ;
+//     minNumder = start ;
+// }
+
+// for (let i = minNumder ; i < minNumder ; i++){
+//     if (i % 2 == 0){
+//         resultsEven += i;
+//     }else{
+//         resultsOdd += i;
+//     }
+// }
+
+// console.log(resultsEven);
+// console.log(resultsOdd);
+
+// let num1 = 0;
+// let num2 = 0;
+
+// for (let i = 1; i <= 30 ; i++){
+//     if ( i % 3 == 0){
+//         num1 += i;
+//     }else{
+//         num2 += i;
+//     }
+// }
+
+// console.log(num1);
+// console.log(num2);
+
+// for (let i = 15; i >= 1 ; i--){
+//     if (i % 4 == 0){
+//         continue;
+//     }else{
+//     console.log(i);
+//     }
+// }
+
+// let nam1 = 0;
+// let results = 0;
+
+// for (nam1 <= 12){
+
+// }
+
+// var result = 0;
+// var pro = Number(prompt('student sany'))
+
+// for (let i = 1; i <= pro; i++) {
+//   if (pro<100 && pro>0) {
+//       var baga = Number(prompt("oqushi bagasi#"+i));
+
+//   while(baga < 0 || baga > 100)
+//     {
+//     if (baga < 0 || baga > 100)
+//   }
+
+//       result += baga/pro;
+
+//   }else{
+//       alert('100 ben 0 arasyn jaz');
+//   }
+// }
+// console.log(result);
+
+// var  nam1;
+// var san = 5;
+// do {
+//     nam1 = prompt("АҚШ презтденті кім ?");
+//     if (nam1 == "Tramp"){
+//     alert("Durys");
+//       alert(san)
+//           break;
+//       }else{
+//          alert('qate');
+//          san --;
+//       }
+//     }while (true)
+
+// alert (san);
+
+let num = 1;
+while (num <= 20) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("fizzbuzz");
+  } else if (num % 3 == 0) {
+    console.log("fizz");
+  } else if (num % 5 == 0) {
+    console.log("buzz");
+  } else {
+    console.log(num);
+  }
+}
